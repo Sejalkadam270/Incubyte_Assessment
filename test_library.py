b3455832aab8a7e7a5b4ce508633d431f9428fce
Sejal <surekhakadam8830@gmail.com>
2024-09-27 06:42:56 +0530

    Add test for borrowing books

--- test_library.py
+++ test_library.py
@@ -1,9 +1,14 @@
-import pytest
-from library import Library, Book
-
-def test_add_book():
+def test_borrow_book():
     library = Library()
     book = Book("978-3-16-148410-0", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
     library.add_book(book)
-    assert len(library.get_available_books()) == 1
-    assert library.get_available_books()[0].title == "The Great Gatsby"
+    assert library.borrow_book("978-3-16-148410-0") == True
+    assert book.is_available == False
+
+def test_borrow_unavailable_book():
+    library = Library()
+    book = Book("978-3-16-148410-0", "The Great Gatsby", "F. Scott Fitzgerald", 1925)
+    library.add_book(book)
+    library.borrow_book("978-3-16-148410-0")
+    with pytest.raises(Exception):
+        library.borrow_book("978-3-16-148410-0")
